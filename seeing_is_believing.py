a0816444839041fdf614258534c5c6d71796ec26
Josh Cheek <josh.cheek@gmail.com>
2013-04-25 09:55:52 -0500

    Add settings

--- seeing_is_believing.py
+++ seeing_is_believing.py
@@ -1,26 +1,43 @@
 import sublime, sublime_plugin, subprocess, os
 
+# when saved, view.file_name() has a value
+# otherwise is None and should be passed on stdin as below
 class YouKnowThatPlaceBetweenSleepAndAwakeThatPlaceWhereYouStillRememberDreamingThatsWhereIllAlwaysLoveYou(sublime_plugin.TextCommand):
   def run(self, edit):
+    # load the text
     region = sublime.Region(0, self.view.size())
     text   = self.view.substr(region)
-    env    = os.environ.copy()
-    env['RBENV_VERSION'] = '2.0.0-p0'
-    s      = subprocess.Popen(
-      ['/Users/joshcheek/.rbenv/shims/ruby', '-S', 'seeing_is_believing',
-          '-Ku',
-          '--result-length', '500',
-      ],
-      env=env,
-      stdin=subprocess.PIPE,
-      stdout=subprocess.PIPE,
-      stderr=subprocess.PIPE
-    )
+
+    # load settings
+    settings = sublime.load_settings("Seeing Is Believing.sublime-settings")
+
+    # set up env vars
+    env = os.environ.copy()
+    for (name, value) in settings.get("environment_variables").iteritems():
+      env[name] = value
+
+    # set up the args
+    args = []
+    args.append(os.path.expanduser(settings.get("ruby_command")))
+    args.append('-S')
+    args.append('seeing_is_believing')
+    for (name, value) in settings.get("flags").iteritems():
+      args.append(str(name))
+      args.append(str(value))
+    if self.view.file_name() != None:
+      args.append("--as")
+      args.append(self.view.file_name())
+
+    # call seeing is believing
+    s = subprocess.Popen(args, env=env, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     out = s.communicate(text.encode('utf-8'))
+
+    # display error
     if s.returncode != None and s.returncode != 0:
       sublime.message_dialog(out[1])
       return
 
+    # replace body with result
     self.view.replace(edit, region, out[0])
 
 class EveryTimeSomeoneSaysIDoNotBelieveInFairiesSomewhereTheresAFairyThatFallsDownDead(sublime_plugin.TextCommand):
