f7d130855d2e4375fc4d13c9f52d61112168bace
Josh Cheek <josh.cheek@gmail.com>
2013-04-27 13:39:51 -0500

    Refactor common code to superclass

--- seeing_is_believing.py
+++ seeing_is_believing.py
@@ -1,6 +1,6 @@
 import sublime, sublime_plugin, subprocess, os
 
-class YouKnowThatPlaceBetweenSleepAndAwakeThatPlaceWhereYouStillRememberDreamingThatsWhereIllAlwaysLoveYou(sublime_plugin.TextCommand):
+class SeeingIsBelieving(sublime_plugin.TextCommand):
   def run(self, edit):
     # load the text
     region = sublime.Region(0, self.view.size())
@@ -19,12 +19,9 @@ class YouKnowThatPlaceBetweenSleepAndAwakeThatPlaceWhereYouStillRememberDreaming
     args.append(os.path.expanduser(settings.get("ruby_command")))
     args.append('-S')
     args.append('seeing_is_believing')
-    for (name, value) in settings.get("flags").iteritems():
-      args.append(str(name))
-      args.append(str(value))
-    if self.view.file_name() != None:
-      args.append("--as")
-      args.append(self.view.file_name())
+
+    # subclass defines this
+    self.setup_flags(args, settings)
 
     # call seeing is believing
     s = subprocess.Popen(args, env=env, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -33,47 +30,32 @@ class YouKnowThatPlaceBetweenSleepAndAwakeThatPlaceWhereYouStillRememberDreaming
     # display error
     #  error code 1 is displayable errors like exceptions getting raised
     #  non zero/one errors can't be displayed, like syntax error, so we need a dialog box
-    if s.returncode != None and s.returncode != 0 and s.returncode != 1:
+    if self.should_display_stderr(s.returncode):
       sublime.message_dialog(out[1])
       return
 
     # replace body with result
     self.view.replace(edit, region, out[0])
 
-class EveryTimeSomeoneSaysIDoNotBelieveInFairiesSomewhereTheresAFairyThatFallsDownDead(sublime_plugin.TextCommand):
-  def run(self, edit):
-    # load the text
-    region = sublime.Region(0, self.view.size())
-    text   = self.view.substr(region)
 
-    # load settings
-    settings = sublime.load_settings("Seeing Is Believing.sublime-settings")
+class YouKnowThatPlaceBetweenSleepAndAwakeThatPlaceWhereYouStillRememberDreamingThatsWhereIllAlwaysLoveYou(SeeingIsBelieving):
+  def setup_flags(self, args, settings):
+    for (name, value) in settings.get("flags").iteritems():
+      args.append(str(name))
+      args.append(str(value))
+    if self.view.file_name() != None:
+      args.append("--as")
+      args.append(self.view.file_name())
 
-    # set up env vars
-    env = os.environ.copy()
-    for (name, value) in settings.get("environment_variables").iteritems():
-      env[name] = value
+  def should_display_stderr(self, returncode):
+    returncode != None and returncode != 0 and returncode != 1
 
-    # set up the args
-    args = []
-    args.append(os.path.expanduser(settings.get("ruby_command")))
-    args.append('-S')
-    args.append('seeing_is_believing')
+class EveryTimeSomeoneSaysIDoNotBelieveInFairiesSomewhereTheresAFairyThatFallsDownDead(SeeingIsBelieving):
+  def setup_flags(self, args, settings):
     args.append('--clean')
     if self.view.file_name() != None:
       args.append("--as")
       args.append(self.view.file_name())
 
-    s = subprocess.Popen(
-      args,
-      env=env,
-      stdin=subprocess.PIPE,
-      stdout=subprocess.PIPE,
-      stderr=subprocess.PIPE
-    )
-    out = s.communicate(text.encode('utf-8'))
-    if s.returncode != None and s.returncode != 0:
-      sublime.message_dialog(out[1])
-      return
-
-    self.view.replace(edit, region, out[0])
+  def should_display_stderr(self, returncode):
+    return returncode != None and returncode != 0
